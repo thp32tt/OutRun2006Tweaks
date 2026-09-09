d0907be957e03c23cbd14fd2709d4925bbe256c6
thp32tt <129367814+thp32tt@users.noreply.github.com>
2026-09-09 23:04:36 +0900

    build(wheel): include direct R3 A/B menu helper

--- src/hooks_wheel_ffb_build.cpp
+++ src/hooks_wheel_ffb_build.cpp
@@ -5,6 +5,7 @@
 #include "hooks_wheel_ffb.cpp"
 #include "hooks_wheel_input_compat_v2.hpp"
 #include "hooks_wheel_r3_menu_dpad.hpp"
+#include "hooks_wheel_r3_menu_ab.hpp"
 #include "hooks_wheel_menu_keyboard_back.hpp"
 #include "hooks_wheel_menu_keyboard_select.hpp"
 #include "hooks_wheel_legacy_blank_defaults.hpp"
