8f11d59d977590632c8e6f585a7a3a1bae65a382
thp32tt <129367814+thp32tt@users.noreply.github.com>
2026-09-21 15:21:46 +0900

    test: add automated VR stage diagnostic profile

--- tools/OutRunVR-TestProfiles.ps1
+++ tools/OutRunVR-TestProfiles.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version Latest
 function Get-OutRunVRTestProfile {
     param(
         [Parameter(Mandatory=$true)]
-        [ValidateSet('CONTROL','CORRECTNESS','PERFORMANCE')]
+        [ValidateSet('CONTROL','CORRECTNESS','PERFORMANCE','STAGE_DIAGNOSTIC')]
         [string]$Name
     )
 
@@ -12,7 +12,11 @@ function Get-OutRunVRTestProfile {
         '-DirectGpuOnly=false',
         '-DisableDesktopDuplication=false',
         '-TargetRefreshRateHz=0',
-        '-SkyGlowFactor=1'
+        '-SkyGlowFactor=1',
+        '-CullingUnionFov=true',
+        '-CullingUnionMarginDegrees=4.0',
+        '-NormalizeReflectionRate=true',
+        '-NearPlane=0.10'
     )
 
     switch ($Name) {
@@ -32,6 +36,29 @@ function Get-OutRunVRTestProfile {
                 Environment=[ordered]@{
                     OUTRUN_VR_TEST_PROFILE='CONTROL'
                     OUTRUN_VR_PERFORMANCE_PROFILE='0'
+                }
+            }
+        }
+        'STAGE_DIAGNOSTIC' {
+            return [ordered]@{
+                Name='STAGE_DIAGNOSTIC'
+                Description='Fast world/effect diagnostic: skip intros, open debug level select and remove race timeout so sky/particle/rival/stage issues can be reproduced without menu traversal.'
+                Arguments=@(
+                    '-FramerateLimit=0',
+                    '-FramerateFastLoad=3',
+                    '-FramerateInterpolation=true',
+                    '-FramerateUnlockExperimental=true',
+                    '-FrameCadenceMode=1',
+                    '-FrameCadenceTargetHz=0',
+                    '-DisableDesktopVsync=true',
+                    '-SkipIntros',
+                    '-OuttaTime',
+                    '-LevelSelect'
+                ) + $commonVr
+                Environment=[ordered]@{
+                    OUTRUN_VR_TEST_PROFILE='STAGE_DIAGNOSTIC'
+                    OUTRUN_VR_PERFORMANCE_PROFILE='0'
+                    OUTRUN_VR_ASSET_DIAGNOSTICS='1'
                 }
             }
         }
