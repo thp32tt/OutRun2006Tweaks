3466cbdb219eb9e14c2c83586e52ab5fa63172e3
thp32tt <129367814+thp32tt@users.noreply.github.com>
2026-09-21 19:12:27 +0900

    fix(vr): cover full Time Attack HUD function range

--- src/vr/hud_semantics.hpp
+++ src/vr/hud_semantics.hpp
@@ -84,7 +84,7 @@ namespace OutRunVRHudSemantics
             return { "C2CDontLoseGF", "HUD_GF_WARNING", SpacePolicy::ScreenHud };
         if (InRange(callRva, 0x0BD900, 0x0BE100))
             return { "GhostGap", "HUD_GHOST", SpacePolicy::ScreenHud };
-        if (InRange(callRva, 0x0BE300, 0x0BEA40))
+        if (InRange(callRva, 0x0BE270, 0x0BEA40))
             return { "DispTimeAttack2D", "HUD_TIME_ATTACK", SpacePolicy::ScreenHud };
         if (InRange(callRva, 0x0BEA40, 0x0BEB20))
             return { "NaviPub_DispTimeAttackGoal", "HUD_GOAL_TIME", SpacePolicy::ScreenHud };
